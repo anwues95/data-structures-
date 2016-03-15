07fba22004ed625885fa10c4c54935259768f24c
SEBASTIAN <SEBASTIAN@192.168.1.4>
2016-03-14 21:51:39 -0500

    homework

--- src/unity1/list.java
+++ src/unity1/list.java
@@ -119,15 +119,68 @@ public class list {
     }
 
     public void duplicatemirror() {
-        
-         
-        
+
+    }
+
+    public void removeDuplicated() {
+        node aux = head, p = head;
+        for (int i = 0; i < count(); i++) {
+            if (i % 2 == 0) {
+                p.next = aux.next;
+                p = aux;
+                aux = aux.next;
+            } else {
+                aux = aux.next;
+            }
+        }
+
+    } public void removeRecurrents(){
+        node aux = head, p = head, back = head;
+        while(aux.next != null){
+            p = head;
+            while(p != null){
+                if(p.data == aux.data){
+                    back.next = aux.next;                    
+                }
+                p = p.next;
+            }
+        }
+        back = aux;
+        aux = aux.next;
     }
     
-        
-        
+
+    public void InsertZeros() {
+        node aux = head;
+        int c = count();
+        for (int i = 1; i < c;) {
+            node n = new node(0);
+            n.next = aux.next;
+            aux.next = n;
+            aux = n.next;
+        }
+    }
+
+    public void removeMiddle() {
+        node aux = head, p = head;
+        int c = count();
+        for (int i = 0; i < c / 2; i++) {
+            p = aux;
+            aux = aux.next;
+        }
+        p.next = aux.next;
+
+    }
+
+    public int countOdds() {
+        node aux = head;
+        int impar = 0;
+        while (aux.next != null) {
+            if (aux.data % 2 == 1) {
+                impar++;
+            }
+            aux = aux.next;
+        }
+        return impar;
+    }
 }
-
-        
-    
-
